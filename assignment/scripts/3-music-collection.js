9c9237d95ad5bfc71514ed3245b7fdb4455b273f
Rickie Natividad <rickienatividad85@gmail.com>
2022-05-31 14:54:25 -0500

    added albums to collection. created showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -9,6 +9,18 @@ function addToCollection(title, artist, yearPublished) {
     return album;
 }
 
-addToCollection('twist','korn', 1997);
+addToCollection('Follow The Leader','Korn', 1998);
+addToCollection('Dirt', 'Alice in Chains', 1992);
+addToCollection('Facelift','Alice in Chains', 1990);
+addToCollection(`Ropin' the Wind`, 'Garth Brooks', 1991);
+addToCollection('Ride the Lightning', 'Metallica', 1984);
+addToCollection(`Peace Sells, but Who's Buying?`, 'Megadeth', 1986);
 console.log(collection);
 
+function showCollection(array){
+    console.log(`There are`, array.length, `items in the list`);
+    for(let i=0; i<array.length; i++){
+        console.log(String(array[i].title), `published by`, String(array[i].artist), `in`, Number(array[i].year));
+    }
+}
+showCollection(collection);

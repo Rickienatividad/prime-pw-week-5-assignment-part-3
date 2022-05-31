a9299acff46f4b7a8db54368e754dc8945299036
Rickie Natividad <rickienatividad85@gmail.com>
2022-05-31 13:05:01 -0500

    workingo on building addToCollection function.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,14 @@
 console.log('***** Music Collection *****')
+let collection = [];
+function addToCollection(title, artist, yearPublished) {
+    let album = {};
+    album.title = [title];
+    album.artist = [artist];
+    album.year = [yearPublished];
+    collection.push(album);
+    return album;
+}
+
+addToCollection('twist','korn', 1997);
+console.log(collection);
+

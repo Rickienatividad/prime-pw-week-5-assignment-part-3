5cc947deba100d786eb6ac00ca6124eff6376110
Rickie Natividad <rickienatividad85@gmail.com>
2022-05-31 16:11:47 -0500

    findByArtist sort of functional.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -20,7 +20,23 @@ console.log(collection);
 function showCollection(array){
     console.log(`There are`, array.length, `items in the list`);
     for(let i=0; i<array.length; i++){
-        console.log(String(array[i].title), `published by`, String(array[i].artist), `in`, Number(array[i].year));
+        console.log(String(array[i].title), `published by`, String(array[i].artist), `in`, String(array[i].year));
     }
 }
 showCollection(collection);
+
+let matchArray = [];
+let emptyArray = [];
+function findByArtist(artist){
+    for(let i=0; i<collection.length; i++){
+        if(String(collection[i].artist) == artist){
+            matchArray.push(String(collection[i].artist));
+            console.log(matchArray);
+        }
+    }
+    if(matchArray.length === 0){
+        console.log(emptyArray);
+    }
+}
+findByArtist('Korn');
+
